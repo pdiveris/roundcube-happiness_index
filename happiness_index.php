f54d8a33dfc3cd5565949e67ae53f124ff9cff76
petrosdiveris <petros@diveris.org>
2024-07-23 13:11:22 +0100

    main: Add Button tests

--- happiness_index.php
+++ happiness_index.php
@@ -34,19 +34,43 @@ class happiness_index extends rcube_plugin
 
         $rcube = rcube::get_instance();
 
+        $this->include_stylesheet($this->local_skin_path() . '/happiness_index.css');
 
-        $this->include_stylesheet($this->local_skin_path() . '/cloud_button.css');
-        $this->add_texts('localization/');
+        $this->add_texts('localization', true);
+        $this->add_button(
+            [
+                'type'     => 'link',
+                'label'    => 'test',
+                'command'  => 'plugin.archive',
+                'class'    => 'button buttonPas archive disabled',
+                'classact' => 'button archive',
+                'width'    => 32,
+                'height'   => 32,
+                'title'    => 'buttontitle',
+                'domain'   => $this->ID,
+                'innerclass' => 'inner',
+            ],
+            'toolbar');
+
+
+        /*
+            add_button() has 2 params the first is an array of properties for the rcmail_output_html::button() function
+            and the second is the name of the container to put the button in.
+
+            supported 'types in the first param are: image, link, input (depreciated), and button (default).
+            You also need a 'command' key and then whatever HTML attributes you want the element to have; id, class etc.          *
+         */
+
 
         $this->add_button(array(
-            'type'       => 'link',
+            'type'       => 'button',
             'label'      => 'happiness_index.happiness',
             'href'       => $this->happiness_index_var,
             'target'     => '_blank',
             'class'      => 'happiness_index',
             'classsel'   => 'happiness_index button-selected',
             'innerclass' => 'button-inner'
-        ), 'taskbar');
+        ), 'toolbar');
 
         // additional email headers
         $additional_headers = [
